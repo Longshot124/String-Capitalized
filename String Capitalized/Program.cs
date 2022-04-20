0e7d2bb23116f651208174597a976cfb62891548
kamra <kamra@LAPTOP-M49P7DNF>
2022-04-20 13:28:16 +0400

    String method added

---
+++ String Capitalized/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace String_Capitalized
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            string text = "bU tasKI metHodla yAzmAliSiz";
+            Console.WriteLine(CultureInfo.CurrentCulture.TextInfo.ToTitleCase(text));
+
+            //var result = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(text);
+            //Console.WriteLine(result);
+
+
+        }
+    }
+}
